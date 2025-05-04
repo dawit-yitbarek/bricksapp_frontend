1f3d0649b1ae18ee6c1942cfc51c3340421bb9cb
dawit-yit <dawityitbarek18@gmail.com>
2025-05-04 21:34:40 +0300

    Telegram login modified 2

--- src/components/TelegramRedirect.jsx
+++ src/components/TelegramRedirect.jsx
@@ -5,30 +5,38 @@ const BackEndUrl = import.meta.env.VITE_BACKEND_URL;
 
 const TelegramRedirect = () => {
   useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const telegramData = {};
-
-    queryParams.forEach((value, key) => {
-      telegramData[key] = value;
-    });
-
-    const connectTelegram = async () => {
-      try {
-        const accessToken = localStorage.getItem("accessToken");
-        const res = await api.post(`${BackEndUrl}/connect-telegram`, { telegramData } , {headers: {Authorization: `Bearer ${accessToken}`,},});
-        window.location.href = res.data.redirect;
-      } catch (err) {
-        console.error("Error:", err);
-        window.location.href = `${FrontEndUrl}/tasks?tgError=failed to connect telegram account`;
-      }
-    };
-    
-    if (telegramData.id) {
-      connectTelegram();
-    }else {
+    const hash = window.location.hash.replace('#tgAuthResult=', '');
+    if (!hash) {
       window.location.href = `${FrontEndUrl}/tasks?tgError=telegram id not found`;
+      return;
+    }
+
+    try {
+      const decoded = JSON.parse(decodeURIComponent(atob(hash)));
+
+      const connectTelegram = async () => {
+        try {
+          const accessToken = localStorage.getItem("accessToken");
+          const res = await api.post(
+            `${BackEndUrl}/connect-telegram`,
+            { telegramData: decoded },
+            { headers: { Authorization: `Bearer ${accessToken}` } }
+          );
+          window.location.href = res.data.redirect;
+        } catch (err) {
+          console.error("Error:", err);
+          window.location.href = `${FrontEndUrl}/tasks?tgError=failed to connect telegram account`;
+        }
+      };
+
+      connectTelegram();
+
+    } catch (err) {
+      console.error("Invalid Telegram auth result:", err);
+      window.location.href = `${FrontEndUrl}/tasks?tgError=invalid auth result`;
     }
   }, []);
+
 
   return (
     <div className="h-screen w-full flex items-center justify-center bg-gray-900 text-white text-lg">
